98035cf409e49f326910a26f48655eecc49aea40
Lexto-arch <jostynhenriquez540@gmail.com>
2024-10-15 20:14:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+/*function toggleMenu() {
+    const barra = document.getElementById("barra");
+    
+
+    barra.classList.toggle("barra-open");
+}
+*/
+
+/*
+
+function toggleMenu() {
+    const barra = document.getElementById('barra');
+    barra.classList.toggle('barra-open');
+}
+
+*/
+
+function toggleMenu() {
+    const barra = document.getElementById('barra');
+    barra.classList.toggle('barra-open'); // Agrega o quita la clase
+}
